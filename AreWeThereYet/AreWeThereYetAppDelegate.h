d5bef58a5ae03775f20744ab03631acfa0093a96
Xiaoyi Cai <sawyer_cai@Xiaoyis-MacBook-Pro.local>
2011-12-13 22:50:05 +0000

    Initial Commit

---
+++ AreWeThereYet/AreWeThereYetAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AreWeThereYetAppDelegate.h
+//  AreWeThereYet
+//
+//  Created by Xiaoyi Cai on 13/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AreWeThereYetAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ AreWeThereYet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AreWeThereYet
+//
+//  Created by Xiaoyi Cai on 13/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AreWeThereYetAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AreWeThereYetAppDelegate class]));
+    }
+}
